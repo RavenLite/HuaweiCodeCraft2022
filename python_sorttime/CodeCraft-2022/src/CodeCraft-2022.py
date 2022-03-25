67ff97707b6d8a4a7566f95419a96e721dded658
BlueStragglers <xczhou2019@gmail.com>
2022-03-25 20:20:57 +0800

    #fix: 修改了一些参数

--- python_sorttime/CodeCraft-2022/src/CodeCraft-2022.py
+++ python_sorttime/CodeCraft-2022/src/CodeCraft-2022.py
@@ -15,6 +15,7 @@ PyPy: pypy3.7-v7.3.7
 numpy: 1.19.4
 """
 
+
 class DataPool:
     """
     Import data from files.
@@ -24,6 +25,7 @@ class DataPool:
     site_bandwidth.csv - max bandwith of each edge (all time)
     demand.csv - demand of each user (by time)
     """
+
     def __init__(self):
         self.read_data()
 
@@ -63,7 +65,7 @@ class DataPool:
         f.close()
 
         return user_list, edge_list, user_edge_list_map, user_edge_qos_map
-    
+
     @staticmethod
     def read_bandwidth() -> dict:
         edge_bandwidth_map = {}
@@ -76,7 +78,7 @@ class DataPool:
         f.close()
 
         return edge_bandwidth_map
-    
+
     def read_demand(self) -> tuple:
         timestamp_key_map: dict[str, int] = {}
         user_demand_list_map: dict[str, list[int]] = {}
@@ -98,9 +100,9 @@ class DataPool:
                     demand_value: int = int(line_splited[index + 1])
                     user_demand_list_map[self.user_list[index]].append(demand_value)
                     time_demand_list_map[row].append(demand_value)
-                
+
                 row += 1
-        
+
         return timestamp_key_map, user_demand_list_map, time_demand_list_map
 
     def read_data(self):
@@ -112,7 +114,7 @@ class DataPool:
         self.edge_count = len(self.edge_list)
         self.timestamp_count = len(list(self.timestamp_key_map.keys()))
         self.free_count = int(self.timestamp_count * 0.05)
-    
+
     def display_data(self):
         pass
         # logging.info("[Reading] max_qos(%s) is %d", type(self.max_qos), self.max_qos)
@@ -134,6 +136,7 @@ class DemandPool:
     timeIndex_edge_left_map - edge left bandwidth (by time)
     edge_free_left - edge left free count (large bandwidth over 95th sorted by time)
     """
+
     def __init__(self, dataPool: DataPool):
         self.timeIndex_user_left_map = {}
         self.timeIndex_edge_left_map = {}
@@ -145,11 +148,12 @@ class DemandPool:
             for edge_qos in list(dataPool.edge_bandwidth_map.keys()):
                 self.timeIndex_edge_left_map[timeIndex][edge_qos] = dataPool.edge_bandwidth_map[edge_qos]
             for user_demand in list(dataPool.user_demand_list_map.keys()):
-                self.timeIndex_user_left_map[timeIndex][user_demand] = dataPool.user_demand_list_map[user_demand][timeIndex]
-        
+                self.timeIndex_user_left_map[timeIndex][user_demand] = dataPool.user_demand_list_map[user_demand][
+                    timeIndex]
+
         for edge in dataPool.edge_list:
             self.edge_free_left[edge] = dataPool.free_count
-        
+
         # logging.info("[DemandPool] timeIndex_edge_left_map(%s) has %d pairs, the first one is %s - %s", type(self.timeIndex_edge_left_map), len(self.timeIndex_edge_left_map.keys()), list(self.timeIndex_edge_left_map.keys())[0], self.timeIndex_edge_left_map[list(self.timeIndex_edge_left_map.keys())[0]])
         # logging.info("[DemandPool] timeIndex_user_left_map(%s) has %d pairs, the first one is %s - %s", type(self.timeIndex_user_left_map), len(self.timeIndex_user_left_map.keys()), list(self.timeIndex_user_left_map.keys())[0], self.timeIndex_user_left_map[list(self.timeIndex_user_left_map.keys())[0]])
 
@@ -164,6 +168,7 @@ class Scheduler:
     meet_demand_by_sorted_edge_timestamp() - phase 1: edge could arrange more, first
     meet_demand_left() - phase 2: arrange left demand, by average
     """
+
     def __init__(self, dataPool: DataPool, demandPool: DemandPool) -> None:
         self.dataPool = dataPool
         self.demandPool = demandPool
@@ -175,7 +180,7 @@ class Scheduler:
             self.res[timeIndex] = {}
             for user in self.dataPool.user_list:
                 self.res[timeIndex][user] = []
-    
+
     def sort_demand_by_edge_timestamp(self):
         self.timeIndex_edge_demand_list.clear()
 
@@ -189,7 +194,8 @@ class Scheduler:
                         sum_demand += self.demandPool.timeIndex_user_left_map[timeIndex][user]
                 self.timeIndex_edge_demand_list.append((timeIndex, edge, sum_demand))
 
-        self.timeIndex_edge_demand_list = sorted(self.timeIndex_edge_demand_list, key=lambda tuple : tuple[2], reverse=True)
+        self.timeIndex_edge_demand_list = sorted(self.timeIndex_edge_demand_list, key=lambda tuple: tuple[2],
+                                                 reverse=True)
         # logging.info("[Scheduling] timeIndex_edge_demand_list(%s) has %d elements, the first one is %s", type(self.timeIndex_edge_demand_list), len(self.timeIndex_edge_demand_list), self.timeIndex_edge_demand_list[0])
 
     def sort_demand_by_user_timestamp(self):
@@ -200,26 +206,14 @@ class Scheduler:
             for user in user_list:
                 user_edge_num_tuple_list.append((user, len(self.dataPool.user_edge_list_map[user])))
 
-            user_edge_num_tuple_list = sorted(user_edge_num_tuple_list, key=lambda tuple : tuple[1])
+            user_edge_num_tuple_list = sorted(user_edge_num_tuple_list, key=lambda tuple: tuple[1])
             user_list_sorted = []
             for user_edge_num_tuple in user_edge_num_tuple_list:
                 user_list_sorted.append(user_edge_num_tuple[0])
 
             self.timeIndex_user_list_sorted_map[timeIndex] = user_list_sorted
 
-    # method one: sort edges by amount of space already used
-    def sort_demand_by_user_edge_list(self, timeIndex: int, user_edge_list: list) -> list:
-        sorted_user_edge_list = []
-        for edge in user_edge_list:
-            if self.demandPool.timeIndex_edge_left_map[timeIndex][edge] <= 0:
-                continue
-            demand = self.dataPool.edge_bandwidth_map[edge] - self.demandPool.timeIndex_edge_left_map[timeIndex][
-                edge]
-            sorted_user_edge_list.append((edge, demand))
-        sorted_user_edge_list = sorted(sorted_user_edge_list, key=lambda tuple: tuple[1])
-        return sorted_user_edge_list
-
-    def meet_demand_by_sorted_edge_timestamp(self, meet_count_max=4):
+    def meet_demand_by_sorted_edge_timestamp(self, meet_count_max=3):
         meet_count = 0
         hasLeftEdge = False
         # edge could provide most at some time, first
@@ -234,10 +228,12 @@ class Scheduler:
             hasmeet = False
             # user with fewer number of available edge under qos, fisrt be arranged
             for user in self.timeIndex_user_list_sorted_map[timeIndex]:
-                if self.dataPool.user_edge_qos_map[user][edge] and self.demandPool.timeIndex_user_left_map[timeIndex][user] > 0:
+                if self.dataPool.user_edge_qos_map[user][edge] and self.demandPool.timeIndex_user_left_map[timeIndex][
+                    user] > 0:
                     meet_num = 0
 
-                    if self.demandPool.timeIndex_edge_left_map[timeIndex][edge] >= self.demandPool.timeIndex_user_left_map[timeIndex][user]:
+                    if self.demandPool.timeIndex_edge_left_map[timeIndex][edge] >= \
+                            self.demandPool.timeIndex_user_left_map[timeIndex][user]:
                         meet_num = self.demandPool.timeIndex_user_left_map[timeIndex][user]
                         self.demandPool.timeIndex_edge_left_map[timeIndex][edge] -= meet_num
                         self.demandPool.timeIndex_user_left_map[timeIndex][user] = 0
@@ -251,7 +247,7 @@ class Scheduler:
 
                     if self.demandPool.timeIndex_edge_left_map[timeIndex][edge] == 0:
                         break
-            
+
             if hasmeet:
                 self.demandPool.edge_free_left[edge] -= 1
                 # simple dynamic sort: from here to the end of this function
@@ -261,8 +257,8 @@ class Scheduler:
 
         # if hasLeftEdge and meet_count == meet_count_max:
         #     self.sort_demand_by_edge_timestamp()
-        #     self.meet_demand_by_sorted_edge_timestamp(meet_count_max * 4)
-    
+        #     self.meet_demand_by_sorted_edge_timestamp(meet_count_max * 3)
+
     def meet_demand_left(self):
         for timeIndex in list(self.demandPool.timeIndex_user_left_map.keys()):
             # user with fewer number of available edge under qos, fisrt be arranged
@@ -271,44 +267,115 @@ class Scheduler:
                     continue
                 edge_meetnum_map = {}
 
-                count = 0
-                user_edge_list = []
                 while self.demandPool.timeIndex_user_left_map[timeIndex][user] > 0:
-                    # user_edge_list = self.dataPool.user_edge_list_map[user]
-                    if count <= 0:
-                        user_edge_list = self.sort_demand_by_user_edge_list(timeIndex, self.dataPool.user_edge_list_map[user])
-                    edge = user_edge_list[count][0]
-                    count = (count + 1) % len(user_edge_list)
-
+                    user_edge_list = self.dataPool.user_edge_list_map[user]
                     # TODO: edge with fewer bandwidth at this time should provide more
-                    # for edge in user_edge_list:
-                    if self.demandPool.timeIndex_edge_left_map[timeIndex][edge] <= 0:
-                        continue
-                    else:
-                        meetnum = 0
-                        if self.demandPool.timeIndex_edge_left_map[timeIndex][edge] < self.demandPool.timeIndex_user_left_map[timeIndex][user]:
-                            meetnum = self.demandPool.timeIndex_edge_left_map[timeIndex][edge]
-                            self.demandPool.timeIndex_edge_left_map[timeIndex][edge] = 0
-                            self.demandPool.timeIndex_user_left_map[timeIndex][user] -= meetnum
+                    for edge in user_edge_list:
+                        if self.demandPool.timeIndex_edge_left_map[timeIndex][edge] <= 0:
+                            continue
                         else:
-                            meetnum = int(self.demandPool.timeIndex_user_left_map[timeIndex][user] * 0.5)
-                            if meetnum == 0:
-                                meetnum = self.demandPool.timeIndex_user_left_map[timeIndex][user]
-                            self.demandPool.timeIndex_edge_left_map[timeIndex][edge] -= meetnum
-                            self.demandPool.timeIndex_user_left_map[timeIndex][user] = self.demandPool.timeIndex_user_left_map[timeIndex][user] - meetnum
+                            meetnum = 0
+                            if self.demandPool.timeIndex_edge_left_map[timeIndex][edge] < \
+                                    self.demandPool.timeIndex_user_left_map[timeIndex][user]:
+                                meetnum = self.demandPool.timeIndex_edge_left_map[timeIndex][edge]
+                                self.demandPool.timeIndex_edge_left_map[timeIndex][edge] = 0
+                                self.demandPool.timeIndex_user_left_map[timeIndex][user] -= meetnum
+                            else:
+                                meetnum = int(self.demandPool.timeIndex_user_left_map[timeIndex][user] * 0.5)
+                                if meetnum == 0:
+                                    meetnum = self.demandPool.timeIndex_user_left_map[timeIndex][user]
+                                self.demandPool.timeIndex_edge_left_map[timeIndex][edge] -= meetnum
+                                self.demandPool.timeIndex_user_left_map[timeIndex][user] = \
+                                self.demandPool.timeIndex_user_left_map[timeIndex][user] - meetnum
 
-                        # avoid repeated addition from the same edge
-                        if edge not in list(edge_meetnum_map.keys()):
-                            edge_meetnum_map[edge] = meetnum
-                        else:
-                            edge_meetnum_map[edge] += meetnum 
+                            # avoid repeated addition from the same edge
+                            if edge not in list(edge_meetnum_map.keys()):
+                                edge_meetnum_map[edge] = meetnum
+                            else:
+                                edge_meetnum_map[edge] += meetnum
 
-                        # if self.demandPool.timeIndex_user_left_map[timeIndex][user] == 0:
-                        #     break
-                
+                            if self.demandPool.timeIndex_user_left_map[timeIndex][user] == 0:
+                                break
+
                 for edge in list(edge_meetnum_map.keys()):
                     self.res[timeIndex][user].append((edge, edge_meetnum_map[edge]))
-        
+
+    def cal_95th_map(self):
+        edge_bandwidth_list_map = {}
+        self.edge_95th_bandwidth_map = {}
+        self.edge_95th_timestamp_map = {}
+        # TODO: manage in one place
+        timeIndex_list = list(self.demandPool.timeIndex_edge_left_map.keys())
+        for timeIndex in timeIndex_list:
+            for edge in self.demandPool.timeIndex_edge_left_map[timeIndex]:
+                if edge not in edge_bandwidth_list_map:
+                    edge_bandwidth_list_map[edge] = []
+                edge_bandwidth_list_map[edge].append(((self.dataPool.edge_bandwidth_map[edge] -
+                                                       self.demandPool.timeIndex_edge_left_map[timeIndex][edge]),
+                                                      timeIndex))
+
+        for edge in list(edge_bandwidth_list_map.keys()):
+            edge_bandwidth_list_map[edge] = sorted(edge_bandwidth_list_map[edge], key=lambda tuple: tuple[0],
+                                                   reverse=True)
+            self.edge_95th_bandwidth_map[edge] = edge_bandwidth_list_map[edge][self.dataPool.free_count][0]
+            self.edge_95th_timestamp_map[edge] = edge_bandwidth_list_map[edge][self.dataPool.free_count][1]
+
+    def optimize_backend(self):
+        self.cal_95th_map()
+        for timeIndex in list(self.res.keys()):
+            for user in list(self.res[timeIndex]):
+                need_swap: bool = False
+                edge_swap = ""
+                bandwidth_swap = 0
+                could_swap: bool = False
+                edge_swaped = ""
+
+                for edge_meetnum in self.res[timeIndex][user]:
+                    edge = edge_meetnum[0]
+                    meetnum = edge_meetnum[1]
+                    # and meetnum > int(self.dataPool.edge_bandwidth_map[edge] * 0.1)
+                    if self.edge_95th_timestamp_map[edge] == timeIndex:
+                        need_swap = True
+                        edge_swap = edge
+                        bandwidth_swap = int(meetnum * 0.025)
+                        break
+
+                if need_swap:
+                    for edge_meetnum in self.res[timeIndex][user]:
+                        edge = edge_meetnum[0]
+                        meetnum = edge_meetnum[1]
+                        # keep not same edge
+                        if edge == edge_swap:
+                            continue
+                        # select the edge with enough space
+                        if (meetnum + bandwidth_swap) < self.edge_95th_bandwidth_map[edge] * 0.95 and \
+                                self.demandPool.timeIndex_edge_left_map[timeIndex][edge] > bandwidth_swap:
+                            could_swap = True
+                            edge_swaped = edge
+                            break
+
+                if could_swap:
+                    # print("时刻: ", timeIndex, " 交换前分配情况: ",  self.res[timeIndex][user])
+                    # print(timeIndex, ": ", edge_swap, edge_swaped)
+                    # print(edge_swap, "'95th is at ", self.edge_95th_timestamp_map[edge_swap], " with value is ", self.edge_95th_bandwidth_map[edge_swap])
+                    # print(edge_swaped, "'95th is at ", self.edge_95th_timestamp_map[edge_swaped], " with value is ", self.edge_95th_bandwidth_map[edge_swaped])
+                    for index, edge_meetnum in enumerate(self.res[timeIndex][user]):
+                        if self.res[timeIndex][user][index][0] == edge_swap:
+                            temp_list = list(edge_meetnum)
+                            temp_list[1] -= bandwidth_swap
+                            self.res[timeIndex][user][index] = tuple(temp_list)
+                            self.demandPool.timeIndex_edge_left_map[timeIndex][edge_swap] += bandwidth_swap
+                        if self.res[timeIndex][user][index][0] == edge_swaped:
+                            temp_list = list(edge_meetnum)
+                            temp_list[1] += bandwidth_swap
+                            self.res[timeIndex][user][index] = tuple(temp_list)
+                            self.demandPool.timeIndex_edge_left_map[timeIndex][edge_swaped] -= bandwidth_swap
+                    self.cal_95th_map()
+                    # print("时刻: ", timeIndex, " 交换后分配情况: ",  self.res[timeIndex][user])
+                    # print(edge_swap, "'95th is at ", self.edge_95th_timestamp_map[edge_swap], " with value is ", self.edge_95th_bandwidth_map[edge_swap])
+                    # print(edge_swaped, "'95th is at ", self.edge_95th_timestamp_map[edge_swaped], " with value is ", self.edge_95th_bandwidth_map[edge_swaped])
+                    # print("\n")
+
     def output(self):
         with open("/output/solution.txt", mode="w", encoding="utf-8") as f:
             for timeIndex in list(self.res.keys()):
@@ -322,10 +389,9 @@ class Scheduler:
                         count += 1
                         if count < timeIndex_user_length:
                             f.write(",")
-                    
+
                     f.write("\n")
         f.close()
-
 
     def schedule(self):
         # prehandle
@@ -336,6 +402,9 @@ class Scheduler:
         self.meet_demand_by_sorted_edge_timestamp()
         # phase 2
         self.meet_demand_left()
+        # phase 3
+        # for index in range(10):
+        # self.optimize_backend()
         # output
         self.output()
 
